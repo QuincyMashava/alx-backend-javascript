a7cf414e14dab8effb7646473f7a57f21d1bc94b
QuincyMashava <mashavaquincy@gmail.com>
2022-08-09 00:44:17 -0700

    initial message

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,5 @@
+const displayMessage = (aMessage) => {
+    console.log(aMessage);
+};
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,3 @@
+Name: Quincy Mashava
+Dir : 0x05-Node_JS_basic
+Msg : Read me for the above mentioned
